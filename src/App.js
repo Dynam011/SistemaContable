6765d78cb46f6ed5ee0587beaa02173fe3e19bb1
Dynam011 <89169227+Dynam011@users.noreply.github.com>
2025-07-08 16:00:37 -0400

    theme changed

--- src/App.js
+++ src/App.js
@@ -23,15 +23,13 @@ const App = () => {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.href.split('?')[1])
     const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
-    if (theme) {
-      setColorMode('light')
-    }
-
+ 
+ setColorMode('light')
     if (isColorModeSet()) {
       return
     }
 
-    setColorMode(storedTheme)
+   
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
